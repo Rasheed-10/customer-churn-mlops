f0d866930a087e1d4f234bdeda83593ff5328bca
Rasheed Aribidesi <rsdaribidesi1429@gmail.com>
2025-06-30 21:27:15 +0100

    EDA on data preprocessing

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,3 +1,13 @@
 import pandas as pd
-df = pd.read_csv('raw-bank-customer-churn/BankChurners dataset.csv')
+from src.download_data import load_csv_from_s3
+
+# Load the data using the function
+df = load_csv_from_s3(
+    bucket_name="bank-customer-churn-bucket",
+    object_key="raw-bank-customer-churn/BankChurners dataset.csv",
+    region_name="eu-north-1"
+)
+
+# 👇 Example: Show column names
+print("Columns in dataset:")
 print(df.columns.tolist())
